dafc8e3e4d4e6870993957138505234641941f3e
BonghoCha <37327313+BonghoCha@users.noreply.github.com>
2022-04-06 22:43:41 +0900

    update: 대양이 봇 코드 추가

---
+++ index.ts
@@ -0,0 +1,28 @@
+import DiscordJS, { Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new DiscordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log("대양이 시동 완료!");
+})
+
+
+var reply_string = ['왜불러', '왜', '응?', '나를 찾느냐', '와이']
+client.on('messageCreate', (message) => {
+    if (message.content === '대양아') {        
+        const rand = Math.floor(Math.random() * 5);
+
+        message.reply({
+            content: reply_string[rand],
+        })
+    }
+})
+
+client.login(process.env.TOKEN)
